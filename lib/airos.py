e4ee33ecf50c9d949be9105240b3464b455d4185
Billy Largent <dirbovine@gmail.com>
2022-06-04 20:37:09 -0600

    Multiprocessing for entire processIPs routine Bug fixed airos.py Changed response type for airos.tryAuth() Introduced device detection (ping) Introduced out to CSV (etc)

--- lib/airos.py
+++ lib/airos.py
@@ -11,9 +11,9 @@ class api:
               'timeout' (default: 10) time to wait before timing out the request
               'verify' (default: False) if true get requests do not verify the ssl certificate
         """
-        result = self.ws.get(url=schema + url + uri, timeout=timeout, verify=verify)
+        result = self.ws.get(url=schema + url + uri,
+                             timeout=timeout, verify=verify)
         return result
-
 
     def __post(self, url: str, uri: str, payload: dict, schema='https://', timeout=10, verify=False):
         """Internal function for a web post request
@@ -24,46 +24,48 @@ class api:
               'timeout' (default: 10) time to wait before timing out the request
               'verify' (default: False) if true get requests do not verify the ssl certificate
         """
-        result = self.ws.post(schema + url + uri, payload, timeout=timeout, verify=verify)
+        try:
+            result = self.ws.post(schema + url + uri, payload,
+                                  timeout=timeout, verify=verify)
+        except requests.exceptions.ConnectionError:
+            return None
         return result
-
 
     def __authMethod(self, ip: ipaddress.IPv4Address):
         """Function for determining the method required for authentication
         Args: 'ip' IPv4 address of the target
-        
+
         returns -1 for error state
         returns 0 for AirOS v4.0
         returns 1 for AirOS v4.1+
         returns 2 for AirOS v8.*
         """
-        
+
         # Send get request to /api/auth different versions of AirOS respond with
         # different http status codes per the version the devices firmware is
-        sc = self.__get(ip, '/api/auth').status_code
-        
+        sc = self.__get(str(ip), '/api/auth').status_code
+
         # Detect estimate firmware version
-        if (sc == 200):
+        if sc == 200:
             # If the http status is 200 this likely means the device is running
             # AirOS v4.0 and does not have a /api/auth uri and redirects to the login page
             return 0
-        elif (sc == 401):
+        elif sc == 401:
             # If the http status is 401 this likely means the device is running
             # AirOS v4.1+ and does not have a /api/auth uri and does not redirect
             # to a login page
 
-            # Required to recieve PROPER cookie as getting /api/auth returns fake cookie 
+            # Required to recieve PROPER cookie as getting /api/auth returns fake cookie
             # on version 4.1 for reasons (I'm not sure possibly a lefover from attempting to
             # then putting off going to the v8 way of /api/auth handling)
-            self.__get(ip, '/login.cgi')
+            self.__get(str(ip), '/login.cgi')
             return 1
-        elif (sc == 403):
+        elif sc == 403:
             # If the https status is 403 this likely means the device is running
             # AirOS v8.* and has a /api/auth uri and will accept creds through it
             return 2
-        else: 
+        else:
             return -1
-
 
     def __validateIP(self, ip: ipaddress.IPv4Address):
         """Function for validating an IP (correct format i.e. 192.168.1.20
@@ -75,11 +77,9 @@ class api:
         except ValueError:
             return False
 
-
     def getStatus(self, ip: ipaddress.IPv4Address):
-        status = self.__get(ip, '/status.cgi').json()
+        status = self.__get(str(ip), '/status.cgi').json()
         return status
-
 
     def tryAuth(self, ip: ipaddress.IPv4Address, username: str, password: str):
         """Function for attepmting authentication with username/password
@@ -87,39 +87,39 @@ class api:
         Args: 'ip' IPv4 address for authentication
         """
         # validate given IPv4 address to avoid errors
-        if (self.__validateIP(ip)):
-            # Call __authMethod for the required detection of different 
-            # AirOS versions and get required session id cooke for 
+        if self.__validateIP(ip):
+            # Call __authMethod for the required detection of different
+            # AirOS versions and get required session id cooke for
             # older versions of AirOS v4.*
             authMethodIndex = self.__authMethod(ip)
-            
+
             # Handle authentication methods
-            if (authMethodIndex == 0 or authMethodIndex == 1):
+            if authMethodIndex == 0 or authMethodIndex == 1:
                 # AirOS v4.0 & AirOS v4.1+
-                self.__post(ip, '/login.cgi', {'username':username, 'password':password})
-                res = self.__get(ip, '/status.cgi')
+                self.__post(str(ip), '/login.cgi',
+                            {'username': username, 'password': password})
+                res = self.__get(str(ip), '/status.cgi')
 
                 # res will not throw a JSONDecodeError if authenticated
                 try:
                     res.json()
-                    return True
+                    return 200
                 except requests.JSONDecodeError:
-                    return False
-            elif (authMethodIndex == 2):
+                    return 403
+            elif authMethodIndex == 2:
                 # AirOS v8.*
-                res = self.__post(ip, '/api/auth', {'username':username, 'password':password}).status_code
-
+                try:
+                    res = self.__post(
+                        str(ip), '/api/auth', {'username': username, 'password': password}).status_code
+                except AttributeError:
+                    res = 404
                 # res will be http status code 200 if authenticated
-                if (res == 200):
-                    return True
-                else:
-                    return False
+                return res
             else:
-                return False
+                return 404
         else:
-            return False
+            return 404
 
-            
     def __init__(self):
         # self.ws is the requests session used for saving cookies on comunications
         self.ws = requests.Session()
--- main.py
+++ main.py
@@ -1,18 +1,284 @@
-from os import system
-from lib.airos import api
+import os
+import csv
+import json
+import ipaddress
+from ping3 import ping  # Needs installed
+from functools import partial
+from lib.airos import api  # Internal API
+from multiprocessing import Pool
 
-# Entry point for the script ---------------------------------------------------------------------
-if (__name__ == '__main__'):
+
+def writeToCSVA(data):
+    file = open('files\\info.csv', 'a', newline='')
+    writer = csv.writer(file)
+    writer.writerows(data)
+    file.close()
+
+
+def writeToCSV(data):
+    file = open('files\\info.csv', 'w', newline='')
+    writer = csv.writer(file)
+    writer.writerows(data)
+    file.close()
+
+
+# Split list into 'equal' parts
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+
+
+# log debug data
+def log(msg, isError=False, isBanner=False):
+    if isBanner:
+        msg = '\n' + msg + '\n'
+    else:
+        if isError:
+            msg = '\n[!] Error: ' + msg + '\n'
+        else:
+            msg = '[+] ' + msg
+
+    logFile = open('files\\debug.log', 'a')
+    logFile.write(msg + '\n')
+    logFile.close
+    print(msg)  # Output message
+
+
+def getAliveIPS(cidr):
+    """ Pings IP's in a CIDR
+    returns the alive IPs
+    """
+    aliveIP = []
+    for p in cidr:
+        alive = ping(str(p))
+        if not(alive):
+            log('The host was not reachable, skipping. (' + str(p) + ')')
+            continue
+        else:
+            log(str(p) + ' is alive.')
+            aliveIP.append(p)
+        if aliveIP != None:
+            return aliveIP
+
+
+# pull json data from .json file stored on disk
+def deserialize(path):
+    try:
+        file = open(path, 'r')
+    except FileNotFoundError as e:
+        log('The requested file: ' + os.getcwd() +
+            '\\' + path + ' was not found.', True)
+        exit()
+    jsonParse = json.load(file)
+    file.close()
+    return jsonParse
+
+
+def processIPs(usernames, passwords, deadIPs, cidr):
+    if cidr == []:
+        return 0
+    data = [[]]
+
+    #print (str(usernames) + '\n\n')
+    #print (str(passwords) + '\n\n')
+
+    # Create api() object as airosapi
     airosapi = api()
 
-    print('AirOS Toolkit')
-    ip = input('IP Address: ')
 
-    if (airosapi.tryAuth(ip, input('Username: '), input('Password: '))):
-        system('cls')
-        print('Authenticated!')
-        host = airosapi.getStatus(ip)['host']
-        print('Firmware: ' + host['fwversion'])
-        print('Model: ' + host['devmodel'])
-    else:
-        print('Incorrect username/password')
+    # Recursively execute code for every IP in cidr
+    for id in range(len(cidr)):
+        # Setup variables
+        alive = True
+        ip = cidr[id]
+        devName:    str = 'N/A'
+        devModel:   str = 'N/A'
+        devFwVer:   str = 'N/A'
+        user:       str = 'N/A'
+        pwID:       int = 0
+        uispStatus: str = 'N/A'
+        httpCode:   int = 999
+
+        # if the IP is dead skip other operations and log this
+        if ip in deadIPs:
+            alive = False
+            httpCode = 502
+
+            data.append([
+                id,          # ID
+                alive,       # Alive IP
+                str(ip),          # IP Address
+                devName,     # Device Name
+                devModel,    # Device Model
+                devFwVer,    # Firmware Version
+                user,        # Username
+                pwID,        # Used Password
+                uispStatus,  # UISP Status
+                httpCode     # HTTP Code
+            ])
+            continue
+
+        # Inform the user of the current ip and index
+        log('Remote IP address: ' + str(ip))
+        log('Index is: ' + str(id))
+        
+        # Bruteforce user list -> pass list
+        auth = False
+        for userr in usernames:
+            if auth:
+                break
+            for pwIDD in range(len(passwords)):
+                # Try to authenticate and get the status of the attempt
+                httpCode = airosapi.tryAuth(ip, userr, passwords[pwIDD])
+                if httpCode == 200:
+                    auth = True
+                if auth:
+                    pwID = pwIDD
+                    user = userr
+                    break
+
+        if auth:
+            log('Authenticated: ' + str(ip))
+        
+            # After authentication get status.cgi
+            statusJson = airosapi.getStatus(ip)
+            httpCode = 200
+
+            # Get data and catch for missing variables
+            try:
+                uispStatus = str(statusJson['unms']['status'])
+            except Exception:
+                uispStatus = 'N/A'
+
+            try:
+                devName = str(statusJson['host']['hostname'])
+            except Exception:
+                devName = 'N/A'
+
+            try:
+                devModel = statusJson['host']['devmodel']
+            except Exception:
+                devModel = 'N/A'
+
+            try:
+                devFwVer = statusJson['host']['fwversion']
+            except Exception:
+                devFwVer = 'N/A'
+
+        # Write data to csv
+        data.append([
+            id,          # ID
+            alive,       # Alive IP
+            str(ip),     # IP Address
+            devName,     # Device Name
+            devModel,    # Device Model
+            devFwVer,    # Firmware Version
+            user,        # Username
+            pwID,        # Used Password
+            uispStatus,  # UISP Status
+            httpCode     # HTTP Code
+        ])
+    
+    data.remove(data[0])
+    return data
+
+# Entry point for the script ---------------------------------------------------------------------
+if __name__ == '__main__':
+    # Clear log file
+    f = open('files\\debug.log', 'w')
+    f.write('')
+    f.close()
+
+    # Welcome message
+    log('--== AirOS Toolkit ==--', False, True)
+
+    log('Reading config...')
+
+    # Get data from config file
+    config = deserialize('files\\config.json')
+
+    # Assign data to variables
+    cidr = config['cidr']
+    poolSize = config['mpPoolSize']
+    passwords = config['auth']['passwords']
+    usernames = config['auth']['usernames']
+
+    log('Done.')
+
+    # Set CSV headers
+    data = [['ID', 'Alive IP', 'IP Address', 'Device Name', 'Device Model',
+             'Firmware Version', 'Username', 'Used Password', 'UISP Status', 'HTTP Code']]
+
+    writeToCSV(data)
+
+    # Seperate the log
+    log('--== Pinging CIDR ==--', False, True)
+
+    # Get cidr from json conf and catch any value errors
+    try:
+        cidr = ipaddress.ip_network(cidr)
+    except ValueError as e:
+        log(str(e), True)
+        exit()
+
+    # Multiprocessing pool setup
+    cidr = list(cidr.hosts())
+    cidr.remove(cidr[0])
+    segment = split(cidr, poolSize)
+
+    # Multiprocessing pool
+    with Pool(poolSize) as p:
+        ret = p.map(getAliveIPS, segment)
+        ret = list([x for x in ret if x is not None])
+
+    # Seperate the log
+    log('--== Ping complete ==--', False, True)
+
+    # Fix ret list (After ping it will be a list of lists list[][])
+    _ret = []
+    for p in range(len(ret)):
+        _ret.append(ret[p][0])
+    ret = _ret
+
+    # Get IPs that did not respond to ping
+    diff1 = set(ret).difference(set(cidr))
+    diff2 = set(cidr).difference(set(ret))
+    deadIPs = list(diff1.union(diff2))
+
+    # Multiprocessing pool setup
+    segment = split(cidr, poolSize)
+    processIPsPartial = partial(processIPs, usernames, passwords, deadIPs)
+
+    # Multiprocessing pool
+    with Pool(poolSize) as p:
+        procReturn = p.map(processIPsPartial, segment)
+        procReturn = list(procReturn)
+
+    log('--== Parsing output ==--', False, True)
+
+    # Get data from the processes and fix it
+    procReturn = list([x for x in procReturn if x != 0])
+    for x in range(len(procReturn)):
+        for v in range(len(procReturn)):
+            try:
+                data.append(procReturn[x][v])
+            except:
+                pass
+    
+    # Sort ip addresses
+    #datab = data
+    #datab.remove(data[0])
+    #data = sorted(datab, key = lambda x: x[2])
+    #print(data)
+
+    for x in range(len(data)):
+        if x != 0:
+            data[x][0] = x - 1
+
+    log('--== Writing data ==--', False, True)
+
+    # write out data
+    writeToCSV(data)
+    
+    # Inform user that execution is complete
+    log('--== Operation complete ==--', False, True)
