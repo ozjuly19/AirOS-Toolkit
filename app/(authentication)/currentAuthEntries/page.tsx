2a0a42d1018c6cbb47373aa8f5fbe73eba78a576
William Largent <w.largent@athr.dev>
2024-11-09 02:00:11 -0700

    Added Current Auth Entries page for checking auth token status and refreshing them if needed.

---
+++ app/(authentication)/currentAuthEntries/page.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { AirOSAuthContext } from "@/src/AirOSLib";
+import { Card, CardContent, Paper, Typography } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import React from "react";
+
+export default function CurrentAuthEntries() {
+  const { AirOSTokens } = React.useContext(AirOSAuthContext);
+
+  React.useEffect(() => {
+    console.log(AirOSTokens);
+  }, [AirOSTokens]);
+
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant='h5'>Currently Authenticated Stations</Typography>
+
+        <Paper sx={{ height: 400, width: '100%' }}>
+          <DataGrid
+            rows={AirOSTokens.map((entry) => {
+              return {
+                id: entry.station_ip,
+                station_ip: entry.station_ip,
+                auth_token: entry.auth_token,
+                isValid: entry.isValid ? 'Current' : 'Expired'
+              };
+            }
+            )}
+
+            columns={[
+              { field: 'station_ip', headerName: 'IP', flex: 1, maxWidth: 150 },
+              { field: 'auth_token', headerName: 'TOKEN', flex: 1 },
+              { field: 'isValid', headerName: 'STATUS', flex: 1 }
+            ]}
+            initialState={{ pagination: { paginationModel: { page: 0, pageSize: 5 } } }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+            sx={{ border: 0 }}
+          />
+        </Paper>
+      </CardContent>
+    </Card>
+  );
+}
