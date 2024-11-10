50bc4f102afa99aad8ffac8a0c02f98f9947a69b
William Largent <w.largent@athr.dev>
2024-11-09 17:52:01 -0700

    Added authenticated GET parameters and updated FetchResponse to use a record type for JSON

--- src/dto/Fetch.dto.ts
+++ src/dto/Fetch.dto.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { AuthTokenType } from "./Authentication.dto";
 
 // Make a static type to represent the response from a fetch request so it can be passed from server -> client
 export const FetchResponse = z.object({
@@ -11,6 +12,13 @@ export const FetchResponse = z.object({
     statusText: z.string(),
     type: z.string(),
     url: z.string(),
-    json: z.any()
+    json: z.record(z.any(), z.any()),
 });
 export type FetchResponseType = z.infer<typeof FetchResponse>;
+
+// Vars required to make authenticated general GET calls to the api
+export const AuthedGetParams = z.object({
+    auth_token: z.custom<AuthTokenType>(),
+    station_ip: z.string().ip({ version: 'v4' }),
+});
+export type AuthedGetParamsType = z.infer<typeof AuthedGetParams>;
